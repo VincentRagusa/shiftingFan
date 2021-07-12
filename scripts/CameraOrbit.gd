d6db497d57a5fc6a60988248dfd6f24d07a7da52
Vincent Ragusa <vincent.ragusa.94@gmail.com>
2021-07-11 20:45:00 -0400

    Q is now allows the player to rotate the camera without rotating their body. This is meant to assist props in carefully positioning themselves without sacreficing the ability to observe the other players.

--- scripts/CameraOrbit.gd
+++ scripts/CameraOrbit.gd
@@ -5,13 +5,24 @@ var lookSensitivity : float = 15.0
 var minLookAngle : float = -20.0
 var maxLookAngle : float = 75.0
 
+var playerCameraLocked = false
+
 var mouseDelta : Vector2 = Vector2()
 
 onready var player = get_parent()
+onready var orbit = get_parent().get_node("CameraOrbit")
 
 func _input(event):	
 	if event is InputEventMouseMotion:
 		mouseDelta = event.relative
+
+func _unhandled_input(event):
+	if event.is_action_pressed("setPlayerRotation"):
+		if playerCameraLocked:
+			player.rotation_degrees.y = orbit.rotation_degrees.y
+			orbit.rotation_degrees.y = 0
+		playerCameraLocked = not playerCameraLocked
+			
 
 func _process(delta):
 	if is_network_master():
@@ -19,7 +30,10 @@ func _process(delta):
 		rotation_degrees.x += rot.x
 		rotation_degrees.x = clamp(rotation_degrees.x, minLookAngle, maxLookAngle)
 		
-		player.rotation_degrees.y -= rot.y
+		if playerCameraLocked:
+			orbit.rotation_degrees.y -= rot.y
+		else:
+			player.rotation_degrees.y -= rot.y
 		
 		mouseDelta  = Vector2()
 	
