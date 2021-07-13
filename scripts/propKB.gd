ecda88f681de6b5af5620a89d85c483810a98901
Vincent Ragusa <vincent.ragusa.94@gmail.com>
2021-07-12 20:21:58 -0400

    update propKB.gd with the new collision transfer method.

--- scripts/propKB.gd
+++ scripts/propKB.gd
@@ -8,17 +8,13 @@ func get_BodyTransform():
 	return get_node("./Body").transform
 	
 
-func get_allCollisionShapesAndTransforms():
-	var shapes = []
-	var transforms = []
-	for c in get_children():
-		if c is CollisionShape:
-			shapes.append(c.shape)
-			transforms.append(c.transform)
-	return [shapes, transforms]
+func get_rigidScene():
+	return get_parent().RigidScene.resource_path
+
 
 func get_height():
 	return self.translation.y
+
 
 func requestPropImpulse(impulse):
 	get_parent()._onKBImpulseRequest(impulse)
